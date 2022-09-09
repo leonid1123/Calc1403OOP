b1688915521ea2d2f582fa0b8e6200c9739c653e
Leo <47742726+leonid1123@users.noreply.github.com>
2022-09-09 11:55:51 +0300

    new class sravnenie

--- Program.cs
+++ Program.cs
@@ -12,43 +12,50 @@ namespace Calc1403
         static void Main(string[] args)
         {
             Oper oper = new Oper();
-            Regex regex = new Regex(@"(\d*)", RegexOptions.IgnoreCase);
-            double i;
-            string d;
-            double j;
+            Regex regex = new Regex(@"^[0-9]+$");
+            Regex regex1 = new Regex(@"(^[+-/*]+$)");
+            double pervoeChislo;
+            string znakDeystviya;
+            double vtoroeChislo;
+            string vvodPolzovatelya; 
             Console.Clear();
             Console.WriteLine("Добро пожаловать в калькулятор. " +
                 "Нажмите Y если хотите продолжить или N чтобы выйти из калькулятора");
+            //сюда дописать выход из калькулятора
             do
             {
-                Console.WriteLine("введите первое число и нажмите ввод");
-
-                ConsoleKeyInfo f = Console.ReadKey();
-
-                //сделать сравнение символов тут
-
-                i = Double.Parse(Console.ReadLine());
-                
-
-                 d = Console.ReadLine();
-
-                 j = Double.Parse(Console.ReadLine());
-
-                if (d.Equals("+"))
+                int proverkaNevernogoVvoda = -1;
+                do
                 {
-                    oper.Slozh(i, j);
+                    proverkaNevernogoVvoda++;
+                    Console.WriteLine("введите первое число и нажмите ввод");
+                    if (i>0) { Console.WriteLine("ты дурачок?"); }
+                    vvodPolzovatelya = Console.ReadLine();
+                } while (!regex.IsMatch(vvodPolzovatelya));//до тех пор пока не даст правильное
+                pervoeChislo = Double.Parse(vvodPolzovatelya);
+                //написать метод для проверки ввода пользователя
+
+
+
+                znakDeystviya = Console.ReadLine();
+
+                vtoroeChislo = Double.Parse(Console.ReadLine());
+
+                if (znakDeystviya.Equals("+"))
+                {
+                    oper.Slozh(pervoeChislo, vtoroeChislo);
                 }
-                else if (d.Equals("-"))
+                else if (znakDeystviya.Equals("-"))
                 {
-                    oper.Vich(i, j);
+                    oper.Vich(pervoeChislo, vtoroeChislo);
                 }
-                else if (d.Equals("*"))
+                else if (znakDeystviya.Equals("*"))
                 {
-                    oper.Umnozh(i, j);
+                    oper.Umnozh(pervoeChislo, vtoroeChislo);
                 }
-                else if (d.Equals("/") & j != 0)
+                else if (znakDeystviya.Equals("/") & vtoroeChislo != 0)
                 {
-                    oper.Deleniye(i, j);
+                    oper.Deleniye(pervoeChislo, vtoroeChislo);
                 }
                 else
                 {
@@ -59,9 +66,17 @@ namespace Calc1403
                 //Console.ReadLine();
             } while (true);
         }
+        public class Proverki
+        {
+
+            public bool ProverkaVvoda(string _vvod, string _sravnenie)
+            {
+
+            }
+        }
         public class Oper
         {
-            public double res;
+            public double res = Double.NaN;
             public void Slozh(double a, double b)
             {
                 res = a + b;
